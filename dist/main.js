a4596e480775179c01e93d2def25b290501f5f89
dadynaderi1 <dadynaderi1@gmail.com>
2022-08-18 00:55:46 +0430

    adding node modules for typescript

---
+++ dist/main.js
@@ -0,0 +1,31 @@
+"use strict";
+class Network {
+    constructor(hostname, port, timeout) {
+        this.port = 80;
+        this.hostname = hostname;
+        this.port = port;
+        this.timeout = timeout;
+    }
+    hostAddressValidator() {
+        const reg = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
+        if (!reg.test(this.hostname)) {
+            throw new Error("host address is invalid");
+        }
+        else if (this.hostname === undefined) {
+            throw new Error("host address is required");
+        }
+        else if (typeof this.hostname !== "string") {
+            throw new TypeError("host address type must be string");
+        }
+        else {
+            return true;
+        }
+    }
+    connect() {
+        if (this.hostAddressValidator()) {
+            console.log("It's okay bro!");
+        }
+    }
+}
+const net = new Network('192.168.1.1', 80, 1000);
+net.connect();
